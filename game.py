57c3c1908bd297b79bbf0af99ee26a19e496ff34
RabbitStar <lavesh.kaushik@moonfroglabs.com>
2017-08-12 12:04:37 +0530

    game file updated

--- game.py
+++ game.py
@@ -1,30 +1,73 @@
-from network_helpers import create_network, load_network, get_stochastic_network_move, save_network
-from moon_toe import random_player, flat_move_to_tuple, emptyboard, Agent, available_moves, apply_move, gameover, \
-    printboard, play_game, state_key
+from network_helpers import get_stochastic_network_move, load_network
+import functools
+from moon_toe import emptyboard, available_moves, flat_move_to_tuple,apply_move,gameover, state_key
+from network_helpers import create_network
 import tensorflow as tf
-import os,collections
 import numpy as np
-from copy import deepcopy
-import random
-import math
-import time
 
+EMPTY = 0
+PLAYER_X = 1
+PLAYER_O = -1
+DRAW = 2
+
+BOARD_SIZE = 10
+LINE_SIZE = 5
+
+NAMES = {0: '_', 1: 'X', -1: 'O'}
+
+def printboard(state):
+    cells = []
+    print ' ',
+    for i in range(BOARD_SIZE):
+        print '{0}'.format(str(i).center(5)),
+    print '\n'
+    for i in range(BOARD_SIZE):
+        print i,
+        for j in range(BOARD_SIZE):
+            print '{0}'.format(NAMES[state[i][j]].center(5)),
+        print('\n')
 
 if __name__ == '__main__':
-    network_file_path = "MoonGo_reinforcement.pickle"
-    target_placeholder = tf.placeholder("float", shape=(None, 1))
-    input_layer, output_layer, variables = create_network(10, (100, 100, 100, 100, 100), output_nodes=1,
-                                                          output_softmax=False)
+    input_layer, output_layer, variables = create_network(10, (10, 10, 10, 10, 10), output_nodes=1,output_softmax=False)
+
     with tf.Session() as session:
         session.run(tf.initialize_all_variables())
-        if network_file_path and os.path.isfile(network_file_path):
-            print("loading pre-existing network")
+        # MoonGo_supervised_cross_prob MoonGo_reinforcement
+        load_network(session, variables, 'MoonGo_reinforcement.pickle')
+        while 1:
             board_state = emptyboard()
-            board_state[0][0] = -1
-            board_state[0][1] = -1
-            board_state[0][2] = -1
-            board_state[0][3] = -1
-            board_state[0][4] = -1
-            load_network(session, variables, network_file_path)
-        ol = session.run(output_layer, feed_dict={input_layer: np.array(state_key(board_state)).reshape(1,10)})
-        print ol
+            player_turn = 1
+
+            while True:
+                printboard(board_state)
+                _available_moves = list(available_moves(board_state))
+
+                if len(_available_moves) == 0:
+                    print("no moves left, game ended a draw")
+                    break
+                if player_turn > 0:
+                    action = raw_input('your move? ')
+                    move = (int(action.split(',')[0]), int(action.split(',')[1]))
+                else:
+                    minval = 999999
+                    move = None
+                    for i in range(BOARD_SIZE):
+                        for j in range(BOARD_SIZE):
+                            if board_state[i][j] == 0:
+                                board_state[i][j] = -1
+                                ol = session.run(output_layer, feed_dict={input_layer: np.array(state_key(board_state)).reshape(1, 10)})
+                                if ol < minval:
+                                    minval = ol
+                                    move = (i,j)
+                                board_state[i][j] = 0
+                if move not in _available_moves:
+                    print("illegal move ", move)
+                    break
+
+                board_state = apply_move(board_state, move, player_turn)
+
+                winner = gameover(board_state)
+                if winner != 0 and winner != 2:
+                    print("we have a winner, side: %s" % player_turn)
+                    break
+                player_turn = -player_turn
